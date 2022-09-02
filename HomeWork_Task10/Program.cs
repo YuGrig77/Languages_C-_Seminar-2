e7d648fd23d8bedcd98cee08a18b304d31adce7c
Grig <chiefy@mail.ru>
2022-09-02 22:35:12 +0300

    Add Home Worrk Task 10

---
+++ HomeWork_Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+Console.Write("Enter numbers: ");
+int a = int.Parse(Console.ReadLine());
+
+int second = (a % 100)/10;
+
+Console.WriteLine($"{a} -> {first}");
