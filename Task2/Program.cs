02d727d52540dfc97d5054b5bcbb51f7cc94154b
Grig <chiefy@mail.ru>
2022-09-02 20:58:45 +0300

    Add Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,9 +7,24 @@
 Console.Clear();
 
 int a = new Random().Next(100, 1000);
-int first = a/100;
-int mid = a/10;
-int last = a-mid*10;
+int first = a/100; // first digit
+int mid = a/10; // second digit
+int last = a-mid*10; // third digit
 
 Console.WriteLine($"{a} -> {first}{last}");
 
+//++Other solution++
+
+//int num = new Random().Next(100, 1000);
+//int a1 = num % 10;
+//int a2 = num / 10;
+//int a3 = num / 100;
+//int X= a1 + a3*10;
+//Console.WriteLine($"{num} - > {X}");
+
+// ++OR++
+
+//int num = new Random().Next(100, 1000);
+//int a1 = num % 10;
+//int a2 = num / 100;
+//Console.WriteLine($"{num} -> {a2 * 10 + a1}");
